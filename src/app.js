4aa4c7a6bf1c8ece8ea7629063856420fc7c9ee1
Dhaval <dhavalchheda@ymail.com>
2017-04-12 10:18:55 +0530

    Addition of logic for routing and Parameter string

--- src/app.js
+++ src/app.js
@@ -11,8 +11,16 @@ app.get('/',function(req,res){
 	res.send("Hello World Again");
 });
 
-app.get('/blog',function(req, res){
-	res.send(posts);
+app.get('/blog/:title?',function(req, res){
+	var title = req.params.title;
+	if (title === undefined) {
+			res.status(503);
+			res.send("This page is under construction");
+	}
+	else {
+			var post = posts[title];
+			res.send(post);
+	}
 });
 app.listen(3000, function(){
 	console.log("The front-end server is running on port 3000!");
